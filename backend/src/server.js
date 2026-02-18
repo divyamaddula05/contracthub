f38366c1ecd1fd8bb67a78b0ad547ee6a69aa8bc
Divya Maddula <divya@example.com>
2026-02-18 13:54:42 +0530

    first upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/../.env" });
+console.log("Using Mongo URI:", process.env.MONGO_URI);
 const app = require("./app");
 const connectDB = require("./config/db");
 const path = require("path");
+
+
 
 
 connectDB();
